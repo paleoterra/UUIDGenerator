f8a689e5f4bb3856034b435c84798fb8bb8386d8
Thomas Moore <sundown@mac.com>
2019-01-20 11:12:06 -0600

    Found that column does not equal character and adjusted post-insertion selection for the presence of tabs

--- UUID Generator/SourceEditorCommand.swift
+++ UUID Generator/SourceEditorCommand.swift
@@ -16,6 +16,8 @@ enum UUIDGenInvocation: String {
 }
 
 class SourceEditorCommand: NSObject, XCSourceEditorCommand {
+
+    static let tabRegex = try? NSRegularExpression.init(pattern: "[\t]", options: [])
     
     func perform(with invocation: XCSourceEditorCommandInvocation, completionHandler: @escaping (Error?) -> Void ) -> Void {
         let selections = invocation.buffer.selections
@@ -35,8 +37,9 @@ class SourceEditorCommand: NSObject, XCSourceEditorCommand {
             let selectedLineRange = NSRange(location: startLine, length: endLine - startLine + 1)
             let selectedLines = lines.subarray(with: selectedLineRange)
             let (newLine, uuidrange) = replace(selectedLines: selectedLines,
-                                  range: textSelection,
-                                  replacementString: generatedUUID(invocation: invocation))
+                                               range: textSelection,
+                                               replacementString: generatedUUID(invocation: invocation),
+                                               tabWidth: invocation.buffer.tabWidth)
             lines.removeObjects(in: selectedLineRange)
             lines.insert(newLine, at: selectedLineRange.location)
 
@@ -56,7 +59,7 @@ class SourceEditorCommand: NSObject, XCSourceEditorCommand {
         return newUuid
     }
 
-    func replace(selectedLines: [Any], range: XCSourceTextRange, replacementString: String) -> (String, NSRange) {
+    func replace(selectedLines: [Any], range: XCSourceTextRange, replacementString: String, tabWidth: Int) -> (String, NSRange) {
         guard let firstLine = selectedLines.first as? String, let lastLine = selectedLines.last as? String else {
             return ("", NSRange(location: 0, length: 0))
         }
@@ -67,7 +70,27 @@ class SourceEditorCommand: NSObject, XCSourceEditorCommand {
         finalString += replacementString
         let lastLineStartIndex = lastLine.index(lastLine.startIndex, offsetBy: range.end.column)
         finalString += String(lastLine[lastLineStartIndex..<lastLine.endIndex])
-        return (finalString, NSRange(location: finalStringStartPosition, length: replacementString.count))
+        let offset = checkTabOffset(line: finalString, column: finalStringStartPosition, tabWidth: tabWidth)
+        return (finalString, NSRange(location: finalStringStartPosition + offset, length: replacementString.count))
+    }
+
+    //since columns != characters, we have to adjust position if tabs present
+    func checkTabOffset(line: String, column: Int, tabWidth: Int) -> Int {
+        guard let regex = SourceEditorCommand.tabRegex else { return 0 }
+        let matches = regex.matches(in: line, options: [], range: NSRange(location: 0, length: line.count))
+        var offset = matches.reduce(0, { (matches, result) -> Int in
+            var count = matches
+            let range = result.range
+            if range.location < column {
+                count += tabWidth
+            }
+            return count
+        })
+        if offset > 0 {
+            offset -= 1
+        }
+        return offset
+
     }
     
 }
